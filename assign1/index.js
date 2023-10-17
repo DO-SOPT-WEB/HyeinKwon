fe862b1b6dee86f92f5b1c9f83a8895415b09f89
HAE2NI <hae0_02ni@ewhain.net>
2023-10-18 00:44:06 +0900

    ✨ 1.a 이미지 호버시 어둡게 처리되며 제목 사진 나타남 구현

---
+++ assign1/index.js
@@ -0,0 +1,30 @@
+import DATA from "./DATA.js";
+
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+// 1.a
+// const firstImages = $(".first_pic_wrapper img");
+const firstImages = document.querySelectorAll(".first_section_pic");
+
+firstImages.forEach((pic, index) => {
+  const h2 = document.createElement("h2");
+  h2.classList.add("title");
+  const p = document.createElement("p");
+  p.classList.add("summary");
+  const { title, summary } = DATA[index];
+  pic.addEventListener("mouseover", function () {
+    pic.childNodes[1].classList.add("addTitle");
+    pic.classList.add("addTitle");
+    h2.innerText = title;
+    pic.appendChild(h2);
+    p.innerText = summary;
+    pic.appendChild(p);
+  });
+
+  pic.addEventListener("mouseleave", function () {
+    pic.classList.remove("addTitle");
+    h2.innerText = "";
+    p.innerText = "";
+  });
+});
