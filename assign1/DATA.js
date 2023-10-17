c1c28ccbc5d0a6baa60baa94b4e7f28eb89b6690
HAE2NI <hae0_02ni@ewhain.net>
2023-10-18 00:43:24 +0900

    🍱 data.js 추가

---
+++ assign1/DATA.js
@@ -0,0 +1,14 @@
+const DATA = [
+  { title: "꽃속의 웰코", summary: "이뿌지!!" },
+  { title: "멀보니웰코", summary: "날씨 조타!" },
+  { title: "눈웃음웰코", summary: "" },
+  { title: "", summary: "" },
+  { title: "", summary: "" },
+  { title: "", summary: "" },
+  { title: "", summary: "" },
+  { title: "", summary: "" },
+  { title: "", summary: "" },
+  { title: "", summary: "" },
+];
+
+export default DATA;
