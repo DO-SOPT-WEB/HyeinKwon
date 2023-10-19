778df50a52c8f0e3cc93302a6b0ce276389082e3
HAE2NI <hae0_02ni@ewhain.net>
2023-10-20 01:19:30 +0900

    🔥 불필요 코드 삭제

--- assign2/DATA.js
+++ assign2/DATA.js
@@ -1,29 +1,27 @@
 //1.a
-export const INIT_BALANCE = 0;
-
 export const HISTORY_LIST = [
   {
     title: "용돈",
-    summary: ["아빠가 용돈을 주었음"],
+    summary: "아빠가 용돈을 주었음",
     money: 150000,
-    inout: income,
+    getto: true,
   },
   {
     title: "과외비",
-    summary: ["과외비"],
+    summary: "과외비",
     money: 700000,
-    inout: income,
+    getto: true,
   },
   {
     title: "의류",
-    summary: ["지그재그"],
+    summary: "지그재그",
     money: 20000,
-    inout: outcome,
+    getto: false,
   },
   {
     title: "식비",
-    summary: ["핵밥 천호점"],
+    summary: "핵밥 천호점",
     money: 36900,
-    inout: outcome,
+    getto: false,
   },
 ];
