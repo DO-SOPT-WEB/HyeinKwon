5d223cea796a02a70fe4f53cf7c189f049e78905
HAE2NI <hae0_02ni@ewhain.net>
2023-10-20 00:22:53 +0900

    🍱 상수 데이터 추가

---
+++ assign2/DATA.js
@@ -0,0 +1,29 @@
+//1.a
+export const INIT_BALANCE = 0;
+
+export const HISTORY_LIST = [
+  {
+    title: "용돈",
+    summary: ["아빠가 용돈을 주었음"],
+    money: 150000,
+    inout: income,
+  },
+  {
+    title: "과외비",
+    summary: ["과외비"],
+    money: 700000,
+    inout: income,
+  },
+  {
+    title: "의류",
+    summary: ["지그재그"],
+    money: 20000,
+    inout: outcome,
+  },
+  {
+    title: "식비",
+    summary: ["핵밥 천호점"],
+    money: 36900,
+    inout: outcome,
+  },
+];
