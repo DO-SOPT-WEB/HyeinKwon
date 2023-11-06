634244df522692691d714a43254f9344944fe3ec
HAE2NI <hae0_02ni@ewhain.net>
2023-11-07 00:15:32 +0900

    🍱 이미지 리스트 추가

---
+++ week3/assign/src/assets/DATA.js
@@ -0,0 +1,112 @@
+const DATA = [
+  {
+    category: "실용",
+    type: "커플",
+    isHuman: true,
+    pic: "https://item.kakaocdn.net/do/87af89cffc6855b58282d2ff2d01831af43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    category: "실용",
+    type: "커플",
+    isHuman: false,
+    pic: "https://item.kakaocdn.net/do/4e607d9b55add2d9107a94998bab13d6f43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    category: "실용",
+    type: "일상",
+    isHuman: true,
+    pic: "https://item.kakaocdn.net/do/ebd4ab21a757555e1eae01cb7e075e707154249a3890514a43687a85e6b6cc82",
+  },
+  {
+    category: "실용",
+    type: "일상",
+    isHuman: false,
+    pic: "https://item.kakaocdn.net/do/3a7872148b08020fd926be93c9400fccf43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    category: "실용",
+    type: "가족",
+    isHuman: true,
+    pic: "https://item.kakaocdn.net/do/d5fd5558cfd9fe7a261b072f1df7b607f43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    category: "실용",
+    type: "가족",
+    isHuman: false,
+    pic: "https://item.kakaocdn.net/do/223f7769560558ad2388d7d07eb248337154249a3890514a43687a85e6b6cc82",
+  },
+
+  {
+    category: "재미",
+    type: "커플",
+    isHuman: true,
+    pic: "https://item.kakaocdn.net/do/f702c7b1e74006db7408788a006d10a4f43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    category: "재미",
+    type: "커플",
+    isHuman: false,
+    pic: "https://item.kakaocdn.net/do/29bb0467618d9fd0aaa133c762b316baf43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    category: "재미",
+    type: "일상",
+    isHuman: true,
+    pic: "https://item.kakaocdn.net/do/0a50095d7cd6c11c1007c283d7cf23abf43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    category: "재미",
+    type: "일상",
+    isHuman: false,
+    pic: "https://item.kakaocdn.net/do/fa47c07d327b29ff2c7d7fcb3331deebf43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    category: "재미",
+    type: "가족/친구",
+    isHuman: true,
+    pic: "https://item.kakaocdn.net/do/e8dca4c1491a4319793309d4a1a6e9b8f43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    category: "재미",
+    type: "가족/친구",
+    isHuman: false,
+    pic: "https://item.kakaocdn.net/do/226c112adbc26b32dea93c28ab78f8427154249a3890514a43687a85e6b6cc82",
+  },
+
+  {
+    category: "독특한",
+    type: "커플",
+    isHuman: true,
+    pic: "https://item.kakaocdn.net/do/613eb86ae3c02ea6d0d60fd46441f94af43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    category: "독특한",
+    type: "커플",
+    isHuman: false,
+    pic: "https://item.kakaocdn.net/do/aab0aad1b73a9eaad9204cf55c43be94f43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    category: "독특한",
+    type: "일상",
+    isHuman: true,
+    pic: "https://item.kakaocdn.net/do/9e39509e677132fe14c0fd7dd3866c1a7154249a3890514a43687a85e6b6cc82",
+  },
+  {
+    category: "독특한",
+    type: "일상",
+    isHuman: false,
+    pic: "https://item.kakaocdn.net/do/e33a110163fc9f2d2475498f6f11463cf43ad912ad8dd55b04db6a64cddaf76d",
+  },
+  {
+    category: "독특한",
+    type: "가족/친구",
+    isHuman: true,
+    pic: "https://item.kakaocdn.net/do/650b31efd243a2ee479fd9ceca63e19e7154249a3890514a43687a85e6b6cc82",
+  },
+  {
+    category: "독특한",
+    type: "가족/친구",
+    isHuman: false,
+    pic: "https://item.kakaocdn.net/do/e39408eaadbe4ec815bead922daa51b6f43ad912ad8dd55b04db6a64cddaf76d",
+  },
+];
