48ad3b212a37fb0c49018a3520a9d81888fe563a
HAE2NI <hae0_02ni@ewhain.net>
2023-11-06 20:46:14 +0900

    ✨ begin project

---
+++ week3/실습/react project/src/components/Counter.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Counter = ({ name }) => {
+  const [number, setNumber] = useState(0);
+
+  const increaseNumber = () => {
+    setNumber(number + 1);
+  };
+
+  const decreaseNumber = () => {
+    setNumber(number - 1);
+  };
+
+  return (
+    <Container>
+      <hi>{name}의 계산기</hi>
+      <p>{number}</p>
+      <ButtonContainer>
+        <CalcButton type="button" onClick={increaseNumber}>
+          +1
+        </CalcButton>
+        <CalcButton type="button" onClick={decreaseNumber}>
+          -1
+        </CalcButton>
+      </ButtonContainer>
+    </Container>
+  );
+};
+
+export default Counter;
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100vw;
+  height: 100vh;
+`;
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 2px;
+`;
+
+const CalcButton = styled.button`
+  background-color: pink;
+  cursor: pointer;
+
+  &:hover {
+    background-color: blue;
+    color: white;
+  }
+`;
