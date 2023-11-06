23dd1f83581069596734e385dd5c1ee90c4aeb15
HAE2NI <hae0_02ni@ewhain.net>
2023-11-06 23:42:09 +0900

    💄 theme, style 변경

--- week3/assign/src/App.jsx
+++ week3/assign/src/App.jsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+import Header from "./components/Header";
+import MainWrapper from "./components/MainWrapper";
+
 function App() {
-  return <></>;
+  return (
+    <Wrapper>
+      <Header />
+      <MainWrapper />
+    </Wrapper>
+  );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 5rem;
+
+  background-color: ${({ theme }) => theme.colors.background};
+`;
--- week3/assign/src/main.jsx
+++ week3/assign/src/main.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./style/theme";
+
+import { GlobalStyle } from "./style/GlobalStyle.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
