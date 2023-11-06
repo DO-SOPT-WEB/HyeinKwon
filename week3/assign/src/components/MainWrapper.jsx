3cce131b1e07926b979ee184c66b7677e941a630
HAE2NI <hae0_02ni@ewhain.net>
2023-11-06 23:42:26 +0900

    ✨ 게임판? 메인 추가

---
+++ week3/assign/src/components/MainWrapper.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+import styled from "styled-components";
+import MainHeader from "./common/MainHeader";
+import ByTypeBtn from "./ByType/ByTypeBtn";
+import ByRandomBtn from "./ByRandom/ByRandomBtn";
+
+export default function MainWrapper() {
+  const [isByType, setIsByType] = useState(false);
+  const [isByRandom, setIsByRandom] = useState(false);
+  const [isStart, setIsStart] = useState(false);
+  function handleStart() {
+    setIsStart(true);
+  }
+
+  return (
+    <Wrapper>
+      {!isStart ? <MainHeader>원하는 추천 방식을 골라줘!</MainHeader> : null}
+      <BtnWrapper>
+        <ByTypeBtn
+          isByType={isByType}
+          setIsByType={setIsByType}
+          isByRandom={isByRandom}
+        />
+        <ByRandomBtn
+          isByRandom={isByRandom}
+          setIsByRandom={setIsByRandom}
+          isByType={isByType}
+          isStart={isStart}
+          handleStart={handleStart}
+        />
+      </BtnWrapper>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 80vw;
+  height: 50vh;
+
+  gap: 5rem;
+  background-color: ${({ theme }) => theme.colors.main};
+`;
+
+const BtnWrapper = styled.article`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  gap: 5rem;
+`;
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: ${(props) => (props.active ? "25rem" : "15rem")};
+  height: 25rem;
+
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.colors.background};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.sub2};
+    ${({ theme }) => theme.fonts.title02};
+  }
+`;
