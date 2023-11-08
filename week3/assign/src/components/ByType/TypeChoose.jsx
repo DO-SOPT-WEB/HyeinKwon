f2006e77615feeba6d96eb69d90550030ccbcaa5
HAE2NI <hae0_02ni@ewhain.net>
2023-11-08 18:59:47 +0900

    ✨ 두번째 단계 페이지 추가

---
+++ week3/assign/src/components/ByType/TypeChoose.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import styled from "styled-components";
+import MainHeader from "../common/MainHeader";
+
+export default function TypeChoose(props) {
+  const { isSelected, setIsSelected } = props;
+  const types = ["커플", "일상", "가족/친구"];
+
+  function handleButtonClick(type) {
+    setIsSelected(type);
+  }
+
+  return (
+    <Wrapper>
+      <MainHeader>어떤 상황에서 쓸 건지 골라봐!</MainHeader>
+      <BtnWrapper>
+        {types.map((type) => {
+          return (
+            <Button
+              key={type}
+              onClick={() => handleButtonClick(type)}
+              active={isSelected === type}>
+              {type}
+            </Button>
+          );
+        })}
+      </BtnWrapper>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  gap: 5rem;
+`;
+
+const BtnWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  gap: 3rem;
+`;
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 20rem;
+  height: 20rem;
+
+  border-radius: 10px;
+  background-color: ${({ theme, active }) =>
+    active ? theme.colors.sub2 : theme.colors.background};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.sub2};
+    ${({ theme }) => theme.fonts.title02};
+  }
+`;
