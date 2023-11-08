5272b6c4e6e4f002273ee86814d48bd9ecf7edeb
HAE2NI <hae0_02ni@ewhain.net>
2023-11-08 21:41:46 +0900

    🔥 쓸모없는 코드 삭제

--- week3/assign/src/components/ByType/ByTypeBtn.jsx
+++ week3/assign/src/components/ByType/ByTypeBtn.jsx
@@ -2,11 +2,18 @@ import styled from "styled-components";
 import StartByType from "./StartByType";
 
 export default function ByTypeBtn(props) {
-  // eslint-disable-next-line react/prop-types
-  const { isByType, isByRandom, setIsByType, isStart, handleStart } = props;
+  const {
+    isByType,
+    isByRandom,
+    setIsByType,
+    isStart,
+    handleStart,
+    setIsFirstStep,
+  } = props;
+
   function handleClick() {
-    console.log("here");
     setIsByType((prev) => !prev);
+    setIsFirstStep(false);
   }
 
   return (
