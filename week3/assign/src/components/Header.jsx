6c86c4116887e1663da1ce28c1af8530b0954e4b
HAE2NI <hae0_02ni@ewhain.net>
2023-11-08 21:41:13 +0900

    ✨ 처음으로 가기 버튼 추가

--- week3/assign/src/components/Header.jsx
+++ week3/assign/src/components/Header.jsx
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
-export default function Header() {
+export default function Header(props) {
+  const { isFirstStep, handleGotoFirstPage } = props;
+
+  console.log(isFirstStep);
+
+  function handleGotoFirstStep() {
+    handleGotoFirstPage();
+  }
+
   return (
     <HeaderWrapper>
       <HeaderTitle>이모티콘 추천해줄게</HeaderTitle>
+      {!isFirstStep && (
+        <GotoFirst onClick={handleGotoFirstStep}>처음으로</GotoFirst>
+      )}
     </HeaderWrapper>
   );
 }
@@ -12,12 +23,32 @@ const HeaderWrapper = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   width: 100%;
   height: 10vh;
-
+  position: relative;
   background-color: ${({ theme }) => theme.colors.main};
 `;
 
 const HeaderTitle = styled.h1`
   ${({ theme }) => theme.fonts.title01};
+`;
+
+const GotoFirst = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 5rem;
+  height: 3rem;
+
+  position: absolute;
+  right: 60rem;
+  top: 5rem;
+
+  background-color: ${({ theme }) => theme.colors.sub2};
+  ${({ theme }) => theme.fonts.body04};
+  border-radius: 5px;
+
+  box-shadow: 2px 2px 2px ${({ theme }) => theme.colors.sub1};
 `;
