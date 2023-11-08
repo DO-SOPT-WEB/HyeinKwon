4ea0b2dad408fdd8951d6a5fc00d6487f41320c2
HAE2NI <hae0_02ni@ewhain.net>
2023-11-08 20:24:15 +0900

    ✨ 결과 화면 추가

---
+++ week3/assign/src/components/ByType/ResultByType.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import { DATA } from "../../assets/DATA";
+
+import MainHeader from "../common/MainHeader";
+
+export default function ResultByType(props) {
+  const { category, type, isHuman } = props;
+
+  function makeResult(array) {
+    return array.filter((ele) => {
+      return (
+        ele.category === category &&
+        ele.type === type &&
+        ele.isHuman === isHuman
+      );
+    });
+  }
+
+  let resultPic = makeResult(DATA);
+
+  return (
+    <Wrapper>
+      <MainHeader>두근두근! 결과는?</MainHeader>
+      {resultPic.map((pic) => (
+        <>
+          <ImgWrapper key={pic.name} src={pic.pic} alt={pic.alt} />
+          <ImgName>{pic.name}</ImgName>
+        </>
+      ))}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+`;
+
+const ImgWrapper = styled.img`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 25rem;
+  height: 25rem;
+`;
+
+const ImgName = styled.h2`
+  text-shadow: 1px 1px 1px ${({ theme }) => theme.colors.sub1};
+
+  padding: 0.5rem;
+  border-top: 3px solid ${({ theme }) => theme.colors.sub1};
+  border-bottom: 3px solid ${({ theme }) => theme.colors.sub1};
+
+  background-color: ${({ theme }) => theme.colors.sub2};
+  ${({ theme }) => theme.fonts.title02};
+`;
