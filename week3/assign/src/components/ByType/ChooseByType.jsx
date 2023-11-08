171e25d7a3c05910370f1825d52193414759e6d5
HAE2NI <hae0_02ni@ewhain.net>
2023-11-08 20:24:30 +0900

    ✨ 취향으로 추천받기 추가

--- week3/assign/src/components/ByType/ChooseByType.jsx
+++ week3/assign/src/components/ByType/ChooseByType.jsx
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 import CategoryChoose from "./CategoryChoose";
 import TypeChoose from "./TypeChoose";
-import AnimalChoose from "./AnimalChoose";
-import { useState } from "react";
+import HumanChoose from "./HumanChoose";
+import { useReducer, useState } from "react";
 import ResultByType from "./ResultByType";
 import MoveToBtn from "../common/MoveToBtn";
 
 export default function ChooseByType({ handleStart }) {
   const [step, setStep] = useState(1);
   const [isSelected, setIsSelected] = useState(null);
+  const [category, setCategory] = useState("");
+  const [type, setType] = useState("");
+  const [isHuman, setIsHuman] = useState(true);
 
   function handleClickNextStep() {
     switch (step) {
@@ -48,15 +51,32 @@ export default function ChooseByType({ handleStart }) {
   return (
     <Wrapper>
       {step == 1 && (
-        <CategoryChoose isSelected={isSelected} setIsSelected={setIsSelected} />
+        <CategoryChoose
+          category={category}
+          setCategory={setCategory}
+          isSelected={isSelected}
+          setIsSelected={setIsSelected}
+        />
       )}
       {step == 2 && (
-        <TypeChoose isSelected={isSelected} setIsSelected={setIsSelected} />
+        <TypeChoose
+          type={type}
+          setType={setType}
+          isSelected={isSelected}
+          setIsSelected={setIsSelected}
+        />
       )}
       {step === 3 && (
-        <AnimalChoose isSelected={isSelected} setIsSelected={setIsSelected} />
+        <HumanChoose
+          isHuman={isHuman}
+          setIsHuman={setIsHuman}
+          isSelected={isSelected}
+          setIsSelected={setIsSelected}
+        />
       )}
-      {step === 4 && <ResultByType />}
+      {step === 4 && (
+        <ResultByType category={category} type={type} isHuman={isHuman} />
+      )}
 
       <BtnWrapper>
         <MoveToBtn
