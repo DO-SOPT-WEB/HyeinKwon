1fe78f9dd45b6d861718c8ef621c009cafa26d55
HAE2NI <hae0_02ni@ewhain.net>
2023-11-15 10:17:47 +0900

    ✨ router 들여오기

---
+++ week4/assign_week4/src/common/Router.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import OnBoarding from "../pages/OnBoarding";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Mypage from "../pages/Mypage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<OnBoarding />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/mypage/:userId" element={<Mypage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
