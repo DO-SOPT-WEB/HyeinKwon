ac85bae0c225d4a504f9b9f5bf60462b28257846
HAE2NI <hae0_02ni@ewhain.net>
2023-11-15 20:21:55 +0900

    ✨ Input 요소 만들기

---
+++ week4/assign_week4/src/components/Login/InputLogin.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import React from "react";
+import { St } from "../../styles/commonStyle";
+
+export default function InputLogin() {
+  return (
+    <>
+      <InputBox />
+    </>
+  );
+}
+
+const InputBox = styled(St.Input)``;
