90d6d1329b4426cccb7005a9e7e0381cb956cd62
HAE2NI <hae0_02ni@ewhain.net>
2023-11-15 21:11:10 +0900

    💄 레이아웃 컴포넌트

---
+++ week4/assign_week4/src/styles/layout/LayoutStyle.jsx
@@ -0,0 +1,8 @@
+import styled, { css } from "styled-components";
+import { St } from "../common/commonStyle";
+export const LayoutStyle = styled.main`
+  ${St.centerFlexColumn};
+  height: 80vh;
+  width: 30vw;
+  gap: 2rem;
+`;
