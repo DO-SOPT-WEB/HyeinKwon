97d648f26981d41e270431c50614d4837420672c
HAE2NI <hae0_02ni@ewhain.net>
2023-11-17 08:59:15 +0900

    🔥 쓸모없는 코드 삭제

--- week4/assign_week4/src/styles/layout/LayoutStyle.jsx
+++ week4/assign_week4/src/styles/layout/LayoutStyle.jsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { St } from "../common/commonStyle";
 export const LayoutStyle = styled.main`
   ${St.centerFlexColumn};
