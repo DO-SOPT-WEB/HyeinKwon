1b2762991f76a722b8d58e8488f956e77086747d
HAE2NI <hae0_02ni@ewhain.net>
2023-11-16 12:02:17 +0900

    💄 theme 요소 추가

--- week4/assign_week4/src/styles/theme.js
+++ week4/assign_week4/src/styles/theme.js
@@ -20,6 +20,7 @@ const fonts = {
   title01: FONT({ weight: 700, size: 2.0, lineHeight: 2.6, letterSpacing: 0 }),
   title02: FONT({ weight: 500, size: 1.6, lineHeight: 2.0, letterSpacing: 0 }),
   body01: FONT({ weight: 500, size: 1.4, lineHeight: 2.0, letterSpacing: 0 }),
+  body02: FONT({ weight: 500, size: 0.8, lineHeight: 1.0, letterSpacing: 0 }),
 };
 
 export const theme = {
