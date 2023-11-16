5ec19c11faa8de3f7662496a81a095e945ee81bb
HAE2NI <hae0_02ni@ewhain.net>
2023-11-16 12:00:21 +0900

    ✨ 회원 가입 버튼

--- week4/assign_week4/src/components/Signup/SignupBtn.jsx
+++ week4/assign_week4/src/components/Signup/SignupBtn.jsx
@@ -1,12 +1,18 @@
-import React from "react";
 import styled from "styled-components";
 import { St } from "../../styles/common/commonStyle";
 
-export default function SignupBtn() {
-  return <SignupButton>회원 가입</SignupButton>;
+export default function SignupBtn(props) {
+  const { disabled, onClick } = props;
+  return (
+    <SignupButton disabled={disabled} onClick={onClick}>
+      회원 가입
+    </SignupButton>
+  );
 }
 
 const SignupButton = styled(St.Button)`
-  background-color: ${({ theme }) => theme.colors.main};
+  background-color: ${({ disabled, theme }) =>
+    disabled ? theme.colors.subTextColor : theme.colors.main};
   color: ${({ theme }) => theme.colors.white};
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
 `;
