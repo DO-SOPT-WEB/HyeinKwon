54cd7494b840e6503795297bdf351020345e131a
HAE2NI <hae0_02ni@ewhain.net>
2023-11-17 08:58:14 +0900

    🚚 main.jsx -> App.jsx로 provider 이동

--- week4/assign_week4/src/App.jsx
+++ week4/assign_week4/src/App.jsx
@@ -1,5 +1,15 @@
+import { ThemeProvider } from "styled-components";
 import Router from "./common/Router";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { theme } from "./styles/theme.js";
+import { Flag } from "./styles/common/commonTextStyle.jsx";
 
 export default function App() {
-  return <Router />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router />
+      <Flag />
+    </ThemeProvider>
+  );
 }
--- week4/assign_week4/src/main.jsx
+++ week4/assign_week4/src/main.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./styles/theme.js";
-import { GlobalStyle } from "./styles/GlobalStyle.js";
-import { Flag } from "./styles/common/commonTextStyle.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Flag />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
