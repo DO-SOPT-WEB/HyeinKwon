4b941c7cdffce3d2c65604ff16343c3ac74b2402
HAE2NI <hae0_02ni@ewhain.net>
2023-11-16 12:00:56 +0900

    ✨ 마이페이지 api 연결

--- week4/assign_week4/src/pages/Mypage.jsx
+++ week4/assign_week4/src/pages/Mypage.jsx
@@ -1,15 +1,40 @@
 import { LayoutStyle } from "../styles/layout/LayoutStyle";
+import { useParams } from "react-router-dom";
 import Header from "../components/common/Header";
 import UserPic from "../components/Mypage/UserPic";
 import UserInform from "../components/Mypage/UserInform";
 import LogoutBtn from "../components/Mypage/LogoutBtn";
+import { useEffect, useState } from "react";
+import axios from "axios";
 export default function Mypage() {
+  const [userData, setUserData] = useState("");
+  const { userId } = useParams();
+
+  const getMypage = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_APP_BASE_URL}/api/v1/members/${userId}`,
+        {
+          userId: userId,
+        }
+      );
+      const { data } = response;
+      setUserData(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getMypage();
+  }, []);
+
   return (
     <LayoutStyle>
       <Header>MY PAGE</Header>
       <UserPic />
-      <UserInform text="ID" />
-      <UserInform text="ID" />
+      <UserInform inform="ID" text={userData.nickname} />
+      <UserInform inform="닉네임" text={userData.username} />
       <LogoutBtn />
     </LayoutStyle>
   );
