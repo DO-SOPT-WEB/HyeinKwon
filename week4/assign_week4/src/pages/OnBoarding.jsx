3a85d5d080819ea9562b187eb25d5484a1632d75
HAE2NI <hae0_02ni@ewhain.net>
2023-11-17 10:14:26 +0900

    💄 온보딩 버튼 꾸미기

--- week4/assign_week4/src/pages/OnBoarding.jsx
+++ week4/assign_week4/src/pages/OnBoarding.jsx
@@ -6,11 +6,21 @@ import { Link } from "react-router-dom";
 export default function OnBoarding() {
   return (
     <LayoutStyle>
-      <GotoLoginButton to="/login">로그인하러가기</GotoLoginButton>
+      <GotoLogin to="/login">
+        <Button>로그인하러가기</Button>
+      </GotoLogin>
     </LayoutStyle>
   );
 }
 
-const GotoLoginButton = styled(Link)`
-  ${St.Button}
+const GotoLogin = styled(Link)``;
+
+const Button = styled.button`
+  width: 30rem;
+  height: 5rem;
+  ${St.centerFlex}
+  border-radius: 10px;
+  ${({ theme }) => theme.fonts.title01};
+  background-color: ${({ theme }) => theme.colors.main};
+  color: ${({ theme }) => theme.colors.white};
 `;
